62d859e8f80b9faf0610fed900bea989ca65f12d
Unknown <anave1@mail.csuchico.edu>
2017-04-06 12:19:40 -0700

    Finished Lab

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,17 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Have you ever", "Million dollars but", "I am", "When can you"];
+  var body = ["touched a cow", "poked a rooster", "Iron Man", "sleep", "squirrels, squirrels, squirrels"];
+  var end =["everywhere", "softly"];
+  randIndex = randomUpTo(intro.length-1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length-1);
+  sentence = sentence + " " + body[randIndex];
+  randIndex = randomUpTo(end.length-1);
+  sentence = sentence + " " + end[randIndex];
+  document.getElementById('tweet').innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
